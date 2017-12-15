7ae9d518e56615a6e3a089af8811bd810d62ae7c
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-12-15 08:49:34 +0100

    Añadido Javadoc

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -1,7 +1,19 @@
 package factorial;
 
+/**
+ * Esta clase contienen el metodo main para la generacion del numero
+ *
+ * @author Arturo 6037
+ */
 public class Factorial{
 
+    /**
+     * El metodo main se encarga de crear un objeto de la clase CalculoNumerico
+     * y de recoger el numero que devuelve el metodo calculoNumero para despues
+     * mostrarlo en pantalla
+     *
+     * @param args
+     */
     public static void main(String[] args){
         int f;
         CalculoNumerico n=new CalculoNumerico();
